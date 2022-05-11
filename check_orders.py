aeb64fb1335813f72ee59c8bd84e57ed2ac44e39
Ron Hudson <pastorhudson@users.noreply.github.com>
2022-05-11 12:51:15 -0400

    debug

--- check_orders.py
+++ check_orders.py
@@ -32,7 +32,7 @@ def get_orders(url, store_name=None):
         data = json.loads(url.read().decode())
     try:
         print(store_name)
-        print(data)
+        print(data['stores'])
         for store in data['stores']:
             if store['store_name'] == store_name:
                 print(f'store {data}')
